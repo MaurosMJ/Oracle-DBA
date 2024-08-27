7b95e9fa053cb5c41daca58dbc7decca7e60e838
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-27 20:07:05 -0300

    feat: Todos os JOBS do banco de dados.sql

---
+++ JOBS/Todos os JOBS do banco de dados.sql
@@ -0,0 +1,15 @@
+--Author: MaurosMJ
+
+/* Scheduler Reports All Jobs */SELECT 
+
+    * 
+
+FROM 
+
+    "SYS"."ALL_SCHEDULER_JOBS" 
+
+WHERE 
+
+    :job_name IS NULL 
+
+    OR upper(job_name) LIKE upper(:job_name) 
