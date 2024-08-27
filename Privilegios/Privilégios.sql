6a7c8ce772817688a9768867829cce29bcb4d71d
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-27 20:28:59 -0300

    feat: Privilégios.sql

---
+++ Privilegios/Privilégios.sql
@@ -0,0 +1,15 @@
+--@Author: MaurosMJ
+
+SELECT 
+
+    privilege             "Privilege", 
+
+    initcap(admin_option) "Admin_Option" 
+
+FROM 
+
+    user_sys_privs 
+
+ORDER BY 
+
+    privilege 
