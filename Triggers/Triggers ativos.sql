0a18f3b7a8c1dbc0034a0c6860c614ad97a60e80
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-27 20:10:14 -0300

    feat: Triggers ativos.sql

---
+++ Triggers/Triggers ativos.sql
@@ -0,0 +1,37 @@
+--Author: MaurosMJ
+
+                          select owner "Owner", 
+
+                                         trigger_name,  
+
+                                         trigger_type,  
+
+                                         table_owner "Table_Owner",  
+
+                                         table_name,  
+
+                                         column_Name,  
+
+                                         triggering_event, 
+
+                                         owner         sdev_link_owner, 
+
+                                         trigger_name  sdev_link_name, 
+
+                                         'TRIGGER'     sdev_link_type 
+
+                                    from sys.dba_triggers 
+
+                                   where (:OWNER is null or owner LIKE upper(:OWNER)) 
+
+                                     and substr(table_name,1,4) != 'BIN$'  
+
+                                     and substr(table_name,1,3) != 'DR$' 
+
+                                     and status = 'ENABLED' 
+
+                                     and (:TABLE_NAME is null or  
+
+                                         upper(table_name) LIKE upper(:TABLE_NAME))  
+
+                                   order by owner, trigger_name 
