3526dba88f893ff5b39afa7a9cbff98cdab21a5e
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-27 19:48:21 -0300

    feat: Sessões inativas.sql

---
+++ Sessões/Sessões inativas.sql
@@ -0,0 +1,42 @@
+--Author: MaurosMJ
+
+WITH vs AS
+     (SELECT sid,
+             status,
+             username,
+             last_call_et,
+             command,
+             machine,
+             osuser,
+             module,
+             action,
+             resource_consumer_group,
+             client_info,
+             client_identifier,
+             type,
+             terminal
+        FROM gv$session
+       WHERE status != 'ACTIVE')
+SELECT vs.sid,
+       vs.username,
+       CASE
+         WHEN vs.status = 'ACTIVE' THEN last_call_et
+         ELSE NULL
+       END AS seconds_in_wait,
+       (SELECT command_name
+          FROM v$sqlcommand
+         WHERE command_type = vs.command) AS "Command",
+       LOWER(vs.machine) AS machine,
+       vs.machine AS machine2,
+       vs.osuser,
+       LOWER(vs.status) AS status,
+       vs.module AS "Module",
+       vs.action AS "Action",
+       vs.resource_consumer_group,
+       vs.client_info,
+       vs.client_identifier
+  FROM vs
+ WHERE vs.username IS NOT NULL
+   AND NVL(vs.osuser, 'x') <> 'SYSTEM'
+   AND vs.type <> 'BACKGROUND'
+ ORDER BY 1;
