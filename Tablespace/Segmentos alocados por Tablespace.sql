6764f55be30f028f33e81e556d73d7dcaadee786
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-27 19:57:03 -0300

    feat: Segmentos alocados por Tablespace.sql

---
+++ Tablespace/Segmentos alocados por Tablespace.sql
@@ -0,0 +1,23 @@
+--Author: MaurosMJ
+
+                       select owner "Owner",  
+
+                                       tablespace_name "Tablespace",  
+
+                                       segment_name "Segment",  
+
+                                       extents "Extents",  
+
+                                       bytes/1024/1024 "Megabytes" 
+
+                                  from sys.dba_segments  
+
+                                 where upper(substr(segment_name,1,4)) != 'BIN$' 
+
+                                   and upper(substr(segment_name,1,3)) != 'DR$' 
+
+                                   and (:TABLESPACE_NAME is null or  
+
+                                       instr(lower(tablespace_name),lower(:TABLESPACE_NAME)) > 0) 
+
+                                 order by owner, extents desc, tablespace_name, segment_name 
