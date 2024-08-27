91bf91bf76be36ca32ad9526075f1d3941f36f5f
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-27 20:20:56 -0300

    feat: Quantidade de objetos por tipo.sql

---
+++ Objetos/Quantidade de objetos por tipo.sql
@@ -0,0 +1,29 @@
+--Author: MaurosMJ
+
+SELECT 
+
+    owner                "Owner", 
+
+    initcap(object_type) "Object Type", 
+
+    COUNT(*)             "Object Count" 
+
+FROM 
+
+    sys.all_objects 
+
+WHERE 
+
+        substr(object_name, 1, 4) != 'BIN$' 
+
+    AND substr(object_name, 1, 3) != 'DR$' 
+
+    AND ( :owner IS NULL 
+
+          OR upper(owner) LIKE upper(:owner) ) 
+
+GROUP BY 
+
+    owner, 
+
+    initcap(object_type) 
