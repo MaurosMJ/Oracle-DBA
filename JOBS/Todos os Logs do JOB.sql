6d063a21935de9e953051f20cd676b6231e7638e
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-27 20:03:54 -0300

    feat: Todos os Logs do JOB.sql

---
+++ JOBS/Todos os Logs do JOB.sql
@@ -0,0 +1,19 @@
+--Author: MaurosMJ
+
+/* Scheduler Reports All Job Logs */SELECT 
+
+    * 
+
+FROM 
+
+    sys.all_scheduler_job_log d 
+
+WHERE 
+
+    :job_name IS NULL 
+
+    OR upper(d.job_name) LIKE upper(:job_name) 
+
+ORDER BY 
+
+    log_id DESC 
