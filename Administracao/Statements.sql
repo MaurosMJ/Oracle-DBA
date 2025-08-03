5487664ec6a8202ec2c5ff231f9c6a4e941dbede
Mauros <73970161+MaurosMJ@users.noreply.github.com>
2025-08-03 11:49:15 -0300

    Create Statements.sql

---
+++ Administracao/Statements.sql
@@ -0,0 +1,8 @@
+-- Determinar se uma instância é parte de um Banco de dados RAC
+SELECT PARALLEL FROM V$INSTANCE;
+
+-- Determinar se o banco de dados está protegido contra perda de dados por um banco de dados Standby
+SELECT PROTECTION_LEVEL FROM V$DATABASE;
+
+-- Determinar se o recurso Streams foi configurado no banco de dados
+SELECT * FROM DBA_STREAMS_ADMINISTRATOR;
